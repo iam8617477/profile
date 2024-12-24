ac57e9f07de094887dd7bdee3c50811d029d3480
iam8617477 <iam8617477@gmail.com>
2024-12-25 02:43:05 +0300

    add file uploads

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 
@@ -26,3 +27,14 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title if self.title else 'Untitled'
+
+
+class File(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import os
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
+from .models import File
 
-# Create your views here.
+
+@login_required
+def download_file(request, file_uuid):
+    file_instance = get_object_or_404(File, uuid=file_uuid)
+    file_path = os.path.join(settings.MEDIA_ROOT, file_instance.file.name)
+
+    if not os.path.exists(file_path):
+        raise Http404('File does not exist')
+
+    file_name = os.path.basename(file_path)
+
+    with open(file_path, 'rb') as f:
+        response = HttpResponse(f.read(), content_type='application/octet-stream')
+        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+
+        return response

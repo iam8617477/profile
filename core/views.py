715f0be16ad7471ef2eaa7d1c1bb77698f7b06f4
iam8617477 <iam8617477@gmail.com>
2024-12-25 03:13:22 +0300

    add system info endpoint

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 import os
+import psutil
+import socket
+import pwd
+
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import File
 
@@ -21,3 +25,36 @@ def download_file(request, file_uuid):
         response['Content-Disposition'] = f'attachment; filename="{file_name}"'
 
         return response
+
+
+@login_required
+def system_info(request):
+    cpu_count = psutil.cpu_count(logical=True)
+    cpu_percent = psutil.cpu_percent(interval=1)
+
+    memory = psutil.virtual_memory()
+    total_ram = round(memory.total / (1024 ** 3), 2)
+    available_ram = round(memory.available / (1024 ** 3), 2)
+
+    disk = psutil.disk_usage('/')
+    total_disk = round(disk.total / (1024 ** 3), 2)
+    free_disk = round(disk.free / (1024 ** 3), 2)
+
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+
+    users = [user.pw_name for user in pwd.getpwall() if user.pw_uid >= 1000]
+
+    data = {
+        'cpu_count': cpu_count,
+        'cpu_percent': cpu_percent,
+        'total_ram': total_ram,
+        'available_ram': available_ram,
+        'total_disk': total_disk,
+        'free_disk': free_disk,
+        'hostname': hostname,
+        'ip_address': ip_address,
+        'users': users,
+    }
+
+    return JsonResponse(data)
